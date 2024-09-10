3a4ab7a779f110926409bb06996f2fbf4d71ed4f
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2024-09-10 17:12:06 +0700

    setup model-views-template

---
+++ main/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    stock = models.PositiveIntegerField(default=0)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    
